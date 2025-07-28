32b89cd125309fe1117c53c8f96ffb40df21bfc2
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-07-28 19:23:12 +0100

    cpp interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,21 @@
-use pyo3::prelude::*;
 mod lcdm;
 
-use lcdm::Likelihood;
+type Likelihood = lcdm::Likelihood;
 
-#[pyfunction]
-pub fn dh_over_rs(z: f64, h0rd: f64, omegam: f64) -> PyResult<f64> {
-    Ok(lcdm::dh_over_rs(z, h0rd, omegam))
-}
-
-#[pyfunction]
-pub fn dm_over_rs(z: f64, h0rd: f64, omegam: f64) -> PyResult<f64> {
-    Ok(lcdm::dm_over_rs(z, h0rd, omegam))
-}
-
-#[pyfunction]
-pub fn dv_over_rs(z: f64, h0rd: f64, omegam: f64) -> PyResult<f64> {
-    Ok(lcdm::dv_over_rs(z, h0rd, omegam))
-}
-
-#[pyclass(name = "Likelihood")]
-struct PyLikelihood {
-    inner: Likelihood,
-}
-
-#[pymethods]
-impl PyLikelihood {
-    #[new]
-    fn new(data_file: &str, cov_file: &str) -> PyResult<Self> {
-        Ok(Self { inner: Likelihood::from_files(data_file, cov_file), })
-    }
-
-    fn __call__(&self, h0rd: f64, omegam: f64) -> PyResult<f64> {
-        Ok(self.inner.logl(h0rd, omegam))
+// export likelihood to C++ interface, leave out the distances
+#[cxx::bridge]
+mod ffi {
+    extern "Rust" {
+        type Likelihood;
+        fn create_likelihood(data_file: &str, cov_file: &str) -> Box<Likelihood>;
+        fn logl(likelihood: &Likelihood, h0rd: f64, omegam: f64) -> f64;
     }
 }
 
-#[pymodule]
-fn deri(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(dh_over_rs, m)?)?;
-    m.add_function(wrap_pyfunction!(dm_over_rs, m)?)?;
-    m.add_function(wrap_pyfunction!(dv_over_rs, m)?)?;
-    m.add_class::<PyLikelihood>()?;
+pub fn create_likelihood(data_file: &str, cov_file: &str) -> Box<lcdm::Likelihood> {
+    Box::new(lcdm::Likelihood::from_files(data_file, cov_file))
+}
 
-    Ok(())
+pub fn logl(likelihood: &lcdm::Likelihood, h0rd: f64, omegam: f64) -> f64 {
+    likelihood.logl(h0rd, omegam)
 }
